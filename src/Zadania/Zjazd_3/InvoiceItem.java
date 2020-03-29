4bd66aa24aff0345fc981a50d97ccfc195f8f57b
s21166-pj <s21166@pjwstk.edu.pl>
2020-03-29 16:30:48 +0200

    Excersise 12 - InvoiceItem Class

---
+++ src/Zadania/Zjazd_3/InvoiceItem.java
@@ -0,0 +1,52 @@
+/*  Summary: Exercise 12 UML CHART InvoiceItem Class construction
+    Author: Badysiak Paweł s21166
+ */
+package Zadania.Zjazd_3;
+
+public class InvoiceItem {
+    private String id;
+    private String desc;
+    private int qty;
+    private double unitPrice;
+
+    public InvoiceItem(String id, String desc, int qty, double unitPrice) {
+        this.id = id;
+        this.desc = desc;
+        this.qty = qty;
+        this.unitPrice = unitPrice;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public int getQty() {
+        return qty;
+    }
+
+    public double getUnitPrice() {
+        return unitPrice;
+    }
+
+    public void setUnitPrice(double unitPrice) {
+        this.unitPrice = unitPrice;
+    }
+
+    public double getTotal() {
+        return unitPrice * qty;
+    }
+
+    @Override
+    public String toString() {
+        return "InvoiceItem{" +
+                "id='" + id + '\'' +
+                ", desc='" + desc + '\'' +
+                ", qty=" + qty +
+                ", unitPrice=" + unitPrice +
+                '}';
+    }
+}
