6473994d3776894727c0defc8407253767d913aa
s21166-pj <s21166@pjwstk.edu.pl>
2020-05-09 17:28:23 +0200

    Exercise 05_05

---
+++ src/Zadania/Zjazd_5/Invoice.java
@@ -0,0 +1,45 @@
+/*
+Summary: Exercise 05_05 UML CHART Invoice Class construction
+Author: Badysiak Paweł s21166
+ */
+package Zadania.Zjazd_5;
+
+public class Invoice {
+    private int ID;
+    private Customer customer;
+    private double amount;
+
+    public Invoice(int ID, Customer customer, double amount) {
+        this.ID = ID;
+        this.customer = customer;
+        this.amount = amount;
+    }
+
+    public int getID() {
+        return ID;
+    }
+
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(Customer customer) {
+        this.customer = customer;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    public String getCustomerName() {
+        return customer.getName();
+    }
+
+    public double getAmountAfterDiscount() {
+        return (amount * customer.getDiscount() / 100) + amount;
+    }
+}
